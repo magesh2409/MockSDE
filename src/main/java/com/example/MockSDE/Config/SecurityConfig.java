7efcffbc4fc781fbbb43b41c467b358d671334e0
magesh <magesh@poshmark.com>
2025-12-27 22:34:07 +0530

    added mongo conf

--- src/main/java/com/example/MockSDE/Config/SecurityConfig.java
+++ src/main/java/com/example/MockSDE/Config/SecurityConfig.java
@@ -1,14 +1,24 @@
 package com.example.MockSDE.Config;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfig {
+@AllArgsConstructor
+public class SecurityConfig extends AbstractMongoClientConfiguration {
+
+    private Environment environment;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -23,5 +33,20 @@ public class SecurityConfig {
                         .anyRequest()
                         .permitAll());
         return http.build();
+    }
+
+
+    @Override
+    public String getDatabaseName() {
+        return environment.getProperty("spring.data.mongodb.database");
+    }
+
+    @Override
+    public MongoClient mongoClient() {
+        final ConnectionString connectionString = new ConnectionString(environment.getProperty("spring.data.mongodb.uri"));
+        final MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .build();
+        return MongoClients.create(mongoClientSettings);
     }
 }
